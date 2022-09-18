1d26cef7fd3233380e22f90c481c0eb33e124dd3
khangtran <tqkhang93@gmail.com>
2022-09-19 04:48:03 +0700

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import './style.css';
+import Mission from './mission';
 
 export default class App extends React.Component {
   state = {
     toggle: false,
     gift: '',
+    ticket: 5,
   };
 
   listGifts = [
-    { des: 'Nạp thẻ Viettel 10K', value: 10 },
-    { des: 'Chúc may mắn lần sau', value: 55 },
-    { des: 'Phiếu mua hàng trị giá 1.000.000đ', value: 2 },
-    { des: 'Giảm 30% Coffee House', value: 8 },
-    { des: '+100 điểm thưởng', value: 25 },
+    { des: 'Bạn nhận được 10.000đ', value: 10, type: 'money' },
+    { des: 'Chúc may mắn lần sau', value: 55, type: 'none' },
+    { des: 'Phiếu mua hàng trị giá 1.000.000đ', value: 2, type: 'voucher' },
+    { des: 'Giảm 30% Coffee House', value: 7, type: 'voucher' },
+    { des: '+20 điểm thưởng', value: 12, type: 'point' },
+    { des: '+100 điểm thưởng', value: 9, type: 'point' },
+    { des: '+200 điểm thưởng', value: 5, type: 'point' },
   ];
 
   img = 'https://cdn.dribbble.com/users/1162379/screenshots/6822916/gift.gif';
@@ -27,37 +31,50 @@ export default class App extends React.Component {
           <div className="row">
             <div></div>
             <div className="title">Hộp quà may mắn</div>
-            <span>túi đồ</span>
+            <div></div>
+          </div>
+        </div>
+        <div className="content">
+          <div className="wrap">
+            {[...Array(9).keys()].map((item, index) => (
+              <div
+                key={index}
+                className="item"
+                onClick={() => this.onClickGift(item)}
+              >
+                <img
+                  className="img"
+                  src="https://raw.githubusercontent.com/khangtran/react-gift/main/public/gift.png"
+                />
+              </div>
+            ))}
+          </div>
+          {this.state.toggle ? (
+            <div className="popup" style={{ display: 'flex' }}>
+              <div className="bg">
+                <img src={this.img} />
+                <span className="gift-text">{this.state.gift}</span>
+              </div>
+            </div>
+          ) : null}
+        </div>
+
+        <div className="float-ticket">
+          <span>Vé: {this.state.ticket} lượt</span>
+        </div>
+
+        <div className="navigation-bar">
+          <div className="content row">
+            <span>Túi đồ</span>
+            <span onClick={() => this.onTabBarMission()}>Nhiệm vụ</span>
+            <span>Thể lệ</span>
           </div>
         </div>
 
-        <div className="row">
-          <span>Thể lệ</span>
-          <span></span>
-        </div>
-
-        <div className="list">
-          {[...Array(9).keys()].map((item, index) => (
-            <div
-              key={index}
-              className="item"
-              onClick={() => this.onClick(item)}
-            >
-              <img
-                className="img"
-                src="https://raw.githubusercontent.com/khangtran/react-gift/main/public/gift.png"
-              />
-            </div>
-          ))}
-        </div>
-        {this.state.toggle ? (
-          <div className="popup" style={{ display: 'flex' }}>
-            <div className="bg">
-              <img src={this.img} />
-              <span className="gift-text">{this.state.gift}</span>
-            </div>
-          </div>
-        ) : null}
+        <Mission
+          ref={(c) => (this.mission = c)}
+          onClick={(value) => this.onUpdateTicket(value)}
+        />
       </div>
     );
   }
@@ -68,41 +85,61 @@ export default class App extends React.Component {
     });
   }
 
-  onClick(item) {
+  onTabBarMission() {
+    this.mission.toggle();
+    console.log('open mission', this.mission);
+  }
+
+  onUpdateTicket(value) {
+    this.setState({
+      ticket: this.state.ticket + value,
+    });
+  }
+
+  onClickGift(item) {
+    if (this.state.ticket <= 0) {
+      alert(
+        'Bạn đã sử dụng hết vé mở hộp. Hãy hoàn thành nhiệm vụ để có thêm vé!'
+      );
+      return;
+    }
     this.togglePopup();
 
     var rnd = this.getRandomInt(0, 100);
     var before = 0;
-    var lst = this.listGifts.map((e, index) => {
+    var _lst = this.listGifts.sort((a, b) => (a.value > b.value ? 1 : -1));
+    console.log({ list: _lst });
+    var lst = _lst.map((e, index) => {
       var v = e.value + before;
-
       before += e.value;
       return { des: e.des, value: v };
     });
 
-    console.log(lst);
+    console.log({ list: lst, random: rnd });
 
+    var before = 0;
+    var gift = '';
+    lst.forEach((e) => {
+      if (gift === '' && e.value > rnd - before) {
+        gift = e.des;
+      }
+    });
     this.timeGift = setTimeout(() => {
       this.setState({
-        gift: rnd,
+        gift: gift,
       });
-    }, 1050);
+    }, 2875);
 
     this.timePopup = setTimeout(() => {
       this.togglePopup();
       this.setState({
         gift: '',
+        ticket: this.state.ticket - 1,
       });
 
       this.timeGift && clearTimeout(this.timeGift);
       this.timePopup && clearTimeout(this.timePopup);
-    }, 1400);
-
-    before = 0;
-    lst.forEach((e) => {
-      if (e.value > rnd - before) {
-      }
-    });
+    }, 3800);
   }
 
   getRandomInt(min, max) {
---
+++ src/mission.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import './style.css';
+
+export default class Mission extends React.Component {
+  state = {
+    toggle: false,
+    list: [
+      { des: 'Đăng nhập mỗi ngày', value: 1, isComplete: true },
+      { des: 'Quy đổi 10.000 điểm thưởng', value: 1, isComplete: false },
+      { des: 'Chia sẽ ứng dụng với bạn bè', value: 1, isComplete: false },
+      {
+        des: 'Mỗi giao dịch thành công qua thẻ Vietinbank',
+        value: 1,
+        max: 3,
+        isComplete: false,
+      },
+    ],
+  };
+
+  toggle() {
+    this.setState({
+      toggle: !this.state.toggle,
+    });
+
+    this.reset();
+  }
+
+  reset() {
+    this.state.list[0].isComplete = true;
+    this.setState({});
+  }
+
+  onClick(item) {
+    if (item.isComplete) this.props.onClick && this.props.onClick(item.value);
+    item.isComplete = false;
+    console.log('click item', this.state.list);
+  }
+
+  render() {
+    return (
+      <div className="root-mission">
+        {this.state.toggle && (
+          <div className="mission">
+            <div className="content">
+              <div className="row">
+                <span className="title">Nhiệm vụ</span>
+                <div
+                  style={{ cursor: 'pointer' }}
+                  onClick={() => this.toggle()}
+                >
+                  <img
+                    src="https://img.icons8.com/material-sharp/344/delete-sign.png"
+                    width={24}
+                    height={24}
+                  />
+                </div>
+              </div>
+              <div className="list">
+                {this.state.list.map((item, index) => (
+                  <div key={index} className="row" style={{ margin: '8px' }}>
+                    <span>{item.des}</span>
+                    <div
+                      className={
+                        'button ' +
+                        (item.isComplete
+                          ? 'bt-item-mission'
+                          : 'bt-item-mission-disable')
+                      }
+                      onClick={() => this.onClick(item)}
+                    >
+                      <span className="button-text">Nhận</span>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
